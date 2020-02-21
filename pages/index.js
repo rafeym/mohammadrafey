40ff4a9134226eccce2c7c8379e49ffdbc47772c
rafeym <mohammadrafey0@gmail.com>
2020-02-21 15:53:15 -0500

    DEVELOP: SEO addition

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Home from '../components/Home/Home'
 import About from '../components/About/About'
+import { Helmet } from 'react-helmet'
 
 const Index = () => {
   return (
     <>
+      <Helmet>
+        <title>Mohammad Rafey | Home </title>
+        <meta
+          name='description'
+          content='Front-End Developer in Toronto, Canada.'
+        />
+      </Helmet>
       <Home />
       <About />
     </>
